729b2e7e28722a5f0712961e2e183fae629518f8
Wonjae Choi <choi@wonj.me>
2023-12-17 16:56:40 +0900

    feat: Update amm liquidity pool structure

--- src/contracts/liquidity_pool.py
+++ src/contracts/liquidity_pool.py
@@ -1,14 +1,11 @@
-from dataclasses import dataclass
-
 from contracts import Contract
 from market.actors.user import User
 from market.token import Token
 
 
-@dataclass
-class LiquidityPoolStorage:
+class LiquidityPool:
     """
-    Represents the storage of a liquidity pool.
+    Represents the a liquidity pool.
 
     Attributes:
         token_1 (Token): The first token in the liquidity pool.
@@ -18,14 +15,28 @@ class LiquidityPoolStorage:
         fee (float): The fee of the liquidity pool.
     """
 
-    token_1: Token
-    token_2: Token
-    reserve_1: float
-    reserve_2: float
-    lp_token: Token
-    fee: float
+    def __init__(
+        self,
+        token_1: Token,
+        token_2: Token,
+        fee: float,
+        reserve_1: float = 0.0,
+        reserve_2: float = 0.0,
+    ):
+        """
+        Initializes a liquidity pool.
 
-    def add_liquidity(self, amount_1: float, amount_2: float) -> None:
+        Args:
+            token_1 (Token): The first token in the liquidity pool.
+            token_2 (Token): The second token in the liquidity pool.
+            fee (float): The fee of the liquidity pool.
+        """
+        self.token_reserve: dict[Token, float] = {}
+        self.token_reserve[token_1] = reserve_1
+        self.token_reserve[token_2] = reserve_2
+        self.fee = fee
+
+    def add_liquidity(self, token: Token, amount: float) -> None:
         """
         Adds liquidity to the liquidity pool.
 
@@ -33,10 +44,9 @@ class LiquidityPoolStorage:
             amount_1 (float): The amount of the first token to add.
             amount_2 (float): The amount of the second token to add.
         """
-        self.reserve_1 += amount_1
-        self.reserve_2 += amount_2
+        self.token_reserve[token] += amount
 
-    def remove_liquidity(self, amount_1: float, amount_2: float) -> None:
+    def remove_liquidity(self, token: Token, amount: float) -> None:
         """
         Removes liquidity from the liquidity pool.
 
@@ -44,13 +54,15 @@ class LiquidityPoolStorage:
             amount_1 (float): The amount of the first token to remove.
             amount_2 (float): The amount of the second token to remove.
         """
-        self.reserve_1 -= amount_1
-        self.reserve_2 -= amount_2
+        if self.token_reserve[token] < amount:
+            raise ValueError("Insufficient funds")
+
+        self.token_reserve[token] -= amount
 
 
-class LiquidityPool(Contract):
+class AmmProtocol(Contract):
     """
-    Represents a liquidity pool for the Diamond Protocol.
+    Represents a AMM DEX protocol.
 
     Attributes:
         name (str): The name of the liquidity pool.
@@ -61,17 +73,9 @@ class LiquidityPool(Contract):
         fee (float): The fee of the liquidity pool.
     """
 
-    def __init__(self, token_1: Token, token_2: Token, fee: float):
-        lp_token = Token("UNI", 0, 0)
-
-        self.pool = LiquidityPoolStorage(
-            token_1=token_1,
-            token_2=token_2,
-            reserve_1=0,
-            reserve_2=0,
-            lp_token=lp_token,
-            fee=fee,
-        )
+    def __init__(self, name: str):
+        self.name = name
+        self.pools: dict[tuple[Token, Token], LiquidityPool] = {}
 
         self.functions = {
             "swap": self.swap,
@@ -79,113 +83,99 @@ class LiquidityPool(Contract):
             "remove_liquidity": self.remove_liquidity,
         }
 
-    @property
-    def name(self) -> str:
-        return f"{self.pool.token_1.name}-{self.pool.token_2.name} LP"
-
-    def swap(self, sender: User, token_in: Token, amount_in: float):
+    def create_pool(
+        self,
+        token_1: Token,
+        token_2: Token,
+        fee: float,
+        reserve_1: float = 0.0,
+        reserve_2: float = 0.0,
+    ) -> LiquidityPool:
         """
-        Swaps a token for another token in the liquidity pool.
+        Creates a liquidity pool.
 
         Args:
-            token_in (Token): The token to swap for another token.
-            amount_in (float): The amount of the token to swap.
-
-        Returns:
-            float: The amount of the other token received.
+            token_1 (Token): The first token in the liquidity pool.
+            token_2 (Token): The second token in the liquidity pool.
+            fee (float): The fee of the liquidity pool.
         """
-        amount_out = self._get_amount_out(token_in, amount_in)
+        if token_1 == token_2:
+            raise ValueError("Tokens must be different")
+
+        if (token_1, token_2) in self.pools or (token_2, token_1) in self.pools:
+            raise ValueError("Liquidity pool already exists")
+
+        new_pool = LiquidityPool(token_1, token_2, fee, reserve_1, reserve_2)
+
+        self.pools[(token_1, token_2)] = new_pool
+
+        return new_pool
+
+    def swap(
+        self, sender: User, token_in: Token, token_out: Token, amount_in: float
+    ) -> float:
+        pool = self._get_pool(token_in, token_out)
+
+        amount_out = self._get_amount_out(pool, token_in, token_out, amount_in)
 
         sender.remove_from_wallet(token_in, amount_in)
-        sender.add_to_wallet(self._get_another_token(token_in), amount_out)
+        pool.add_liquidity(token_in, amount_in)
 
-        if token_in == self.pool.token_1:
-            self.pool.reserve_1 += amount_in
-            self.pool.reserve_2 -= amount_out
-        elif token_in == self.pool.token_2:
-            self.pool.reserve_2 += amount_in
-            self.pool.reserve_1 -= amount_out
-        else:
-            raise ValueError("Token not in liquidity pool")
+        pool.remove_liquidity(token_out, amount_out)
+        sender.add_to_wallet(token_out, amount_out)
 
         return amount_out
 
-    def add_liquidity(self, sender: User, amount_1: float, amount_2: float):
-        """
-        Adds liquidity to the liquidity pool.
+    def add_liquidity(
+        self,
+        sender: User,
+        token_1: Token,
+        token_2: Token,
+        amount_1: float,
+        amount_2: float,
+    ):
+        pool = self._get_pool(token_1, token_2)
 
-        Args:
-            amount_1 (float): The amount of the first token to add.
-            amount_2 (float): The amount of the second token to add.
+        sender.remove_from_wallet(token_1, amount_1)
+        sender.remove_from_wallet(token_2, amount_2)
 
-        Returns:
-            float: The amount of liquidity tokens minted.
-        """
-        sender.remove_from_wallet(self.pool.token_1, amount_1)
-        sender.remove_from_wallet(self.pool.token_2, amount_2)
+        pool.add_liquidity(token_1, amount_1)
+        pool.add_liquidity(token_2, amount_2)
 
-        self.pool.add_liquidity(amount_1, amount_2)
+    def remove_liquidity(
+        self,
+        sender: User,
+        token_1: Token,
+        token_2: Token,
+        amount_1: float,
+        amount_2: float,
+    ):
+        pool = self._get_pool(token_1, token_2)
 
-        sender.add_to_wallet(self.pool.lp_token, amount_1 + amount_2)
+        pool.remove_liquidity(token_1, amount_1)
+        pool.remove_liquidity(token_2, amount_2)
 
-    def remove_liquidity(self, sender: User, amount_1: float, amount_2: float):
-        """
-        Removes liquidity from the liquidity pool.
+        sender.add_to_wallet(token_1, amount_1)
+        sender.add_to_wallet(token_2, amount_2)
 
-        Args:
-            amount_1 (float): The amount of the first token to remove.
-            amount_2 (float): The amount of the second token to remove.
-
-        Returns:
-            float: The amount of liquidity tokens burned.
-        """
-        if self.pool.reserve_1 < amount_1 or self.pool.reserve_2 < amount_2:
-            raise ValueError("Insufficient funds")
-
-        sender.add_to_wallet(self.pool.token_1, amount_1)
-        sender.add_to_wallet(self.pool.token_2, amount_2)
-
-        self.pool.remove_liquidity(amount_1, amount_2)
-
-        sender.wallet[self.pool.lp_token] -= amount_1 + amount_2
-
-    def _get_another_token(self, token: Token) -> Token:
-        """
-        Gets the other token in the liquidity pool.
-
-        Args:
-            token (Token): The token to get the other token for.
-
-        Returns:
-            Token: The other token in the liquidity pool.
-        """
-        if token == self.pool.token_1:
-            return self.pool.token_2
-
-        if token == self.pool.token_2:
-            return self.pool.token_1
-
-        raise ValueError("Token not in liquidity pool")
-
-    def _get_amount_out(self, token_in: Token, amount_in: float) -> float:
-        """
-        Gets the amount of the other token received from a swap.
-
-        Args:
-            amount_in (float): The amount of the token to swap.
-
-        Returns:
-            float: The amount of the other token received.
-        """
-        if token_in == self.pool.token_1:
-            reserve_in = self.pool.reserve_1
-            reserve_out = self.pool.reserve_2
+    def _get_pool(self, token_1: Token, token_2: Token) -> LiquidityPool:
+        if (token_1, token_2) in self.pools:
+            pool = self.pools[(token_1, token_2)]
+        elif (token_2, token_1) in self.pools:
+            pool = self.pools[(token_2, token_1)]
         else:
-            reserve_in = self.pool.reserve_2
-            reserve_out = self.pool.reserve_1
+            raise ValueError("Liquidity pool does not exist")
 
-        amount_in_with_fee = amount_in * (1 - self.pool.fee)
-        return (amount_in_with_fee * reserve_out) / (reserve_in + amount_in_with_fee)
+        return pool
 
-    def __str__(self) -> str:
-        return f"{self.name}:\n\t{self.pool.token_1.name}: {self.pool.reserve_1}\n\t{self.pool.token_2.name}: {self.pool.reserve_2}\n\tLiquidity Tokens: {self.pool.lp_token.total_supply}"
+    def _get_amount_out(
+        self, pool: LiquidityPool, token_in: Token, token_out: Token, amount_in: float
+    ) -> float:
+        reserve_in = pool.token_reserve[token_in]
+        reserve_out = pool.token_reserve[token_out]
+
+        amount_in_with_fee = amount_in * (1 - pool.fee)
+        amount_out = (
+            amount_in_with_fee * reserve_out / (reserve_in + amount_in_with_fee)
+        )
+        return amount_out
---
+++ src/contracts/uniswap_v2.py
@@ -0,0 +1,23 @@
+from contracts.liquidity_pool import AmmProtocol, LiquidityPool
+from market.token import Token
+
+
+class UniswapV2(AmmProtocol):
+    """
+    Represents a Uniswap V2 liquidity pool.
+    """
+
+    def __init__(self, name: str = "UniswapV2"):
+        super().__init__(name)
+
+    def _get_amount_out(
+        self, pool: LiquidityPool, token_in: Token, token_out: Token, amount_in: float
+    ) -> float:
+        reserve_in = pool.token_reserve[token_in]
+        reserve_out = pool.token_reserve[token_out]
+
+        amount_in_with_fee = amount_in * (1 - pool.fee)
+        amount_out = (
+            amount_in_with_fee * reserve_out / (reserve_in + amount_in_with_fee)
+        )
+        return amount_out
